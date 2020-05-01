479578cefd85bb198633a9719ad0e7588825d5ca
Michael Kavas <michaelk0106@gmail.com>
2020-04-30 22:22:39 -0500

    video 7

---
+++ parallel/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '75f62a75a4cf2bb9bbfd6e7471ca60d4'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from parallel import routes
